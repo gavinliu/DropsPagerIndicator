7d66999a098c9eb88e702a87753d5dc984119537
刘云龙 <gavin6liu@gmail.com>
2015-04-10 15:49:29 +0800

    add indicator and demo

--- Library/src/main/java/cn/gavinliu/lib/android/pagerindicator/DropsPagerIndicator.java
+++ Library/src/main/java/cn/gavinliu/lib/android/pagerindicator/DropsPagerIndicator.java
@@ -1,7 +1,289 @@
 package cn.gavinliu.lib.android.pagerindicator;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gavin on 15-4-10.
  */
-public class DropsPagerIndicator {
+public class DropsPagerIndicator extends View {
+
+    private List<Integer> mColors;
+
+    private ArrayList<ValueAnimator> mAnimators;
+
+    private int mWidth;
+    private int mHeight;
+
+    private float leftCircleRadius;
+    private float leftCircleX;
+
+    private float rightCircleRadius;
+    private float rightCircleX;
+
+    private List<PointF> mPoints;
+
+    private int mPagerCount;
+
+    private float mMaxCircleRadius;
+    private float mMinCircleRadius;
+
+    private Paint mPaint;
+    private Path mPath = new Path();
+
+    private Mode mMode = Mode.Normal;
+
+    public enum Mode {
+        Normal, Bend
+    }
+
+    public DropsPagerIndicator(Context context) {
+        this(context, null);
+    }
+
+    public DropsPagerIndicator(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DropsPagerIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mAnimators = new ArrayList<>();
+        mPoints = new ArrayList<>();
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawCircle(leftCircleX, mHeight / 2, leftCircleRadius, mPaint);
+        canvas.drawCircle(rightCircleX, mHeight / 2, rightCircleRadius, mPaint);
+
+        switch (mMode) {
+            case Normal:
+                drawModeNormal(canvas);
+                break;
+
+            case Bend:
+                drawModeBend(canvas);
+                break;
+        }
+    }
+
+    private void drawModeNormal(Canvas canvas) {
+        mPath.reset();
+
+        mPath.moveTo(rightCircleX, mHeight / 2);
+
+        mPath.lineTo(rightCircleX, mHeight / 2 - rightCircleRadius);
+
+        mPath.quadTo(rightCircleX,
+                mHeight / 2 - rightCircleRadius,
+
+                leftCircleX,
+                mHeight / 2 - leftCircleRadius);
+
+        mPath.lineTo(leftCircleX, mHeight / 2 + leftCircleRadius);
+
+        mPath.quadTo(leftCircleX,
+                mHeight / 2 + leftCircleRadius,
+
+                rightCircleX,
+                mHeight / 2 + rightCircleRadius);
+
+        mPath.close();
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    private void drawModeBend(Canvas canvas) {
+        float middleOffset = (leftCircleX - rightCircleX) / (mPoints.get(1).x - mPoints.get(0).x) * (mHeight / 10);
+
+        mPath.reset();
+
+        mPath.moveTo(rightCircleX, mHeight / 2);
+
+        mPath.lineTo(rightCircleX, mHeight / 2 - rightCircleRadius);
+
+        mPath.cubicTo(rightCircleX,
+                mHeight / 2 - rightCircleRadius,
+
+                rightCircleX + (leftCircleX - rightCircleX) / 2.0F,
+                mHeight / 2 + middleOffset,
+
+                leftCircleX,
+                mHeight / 2 - leftCircleRadius);
+
+        mPath.lineTo(leftCircleX, mHeight / 2 + leftCircleRadius);
+
+        mPath.cubicTo(leftCircleX,
+                mHeight / 2 + leftCircleRadius,
+
+                rightCircleX + (leftCircleX - rightCircleX) / 2.0F,
+                mHeight / 2 - middleOffset,
+
+                rightCircleX,
+                mHeight / 2 + rightCircleRadius);
+
+        mPath.close();
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+
+        mMaxCircleRadius = 0.45f * mHeight;
+        mMinCircleRadius = 0.15f * mHeight;
+
+        resetPoint();
+    }
+
+    private void createAnimator(int position) {
+        if (mPoints.isEmpty()) {
+            return;
+        }
+        mAnimators.clear();
+
+        int next = Math.min(mPagerCount - 1, position + 1);
+
+        float leftX = mPoints.get(position).x;
+        float rightX = mPoints.get(next).x;
+
+        ObjectAnimator rightPointAnimator = ObjectAnimator.ofFloat(this, "rightCircleX", leftX, rightX);
+        rightPointAnimator.setDuration(5000L);
+        rightPointAnimator.setInterpolator(new DecelerateInterpolator(0.8F));
+        mAnimators.add(rightPointAnimator);
+
+        ObjectAnimator leftPointAnimator = ObjectAnimator.ofFloat(this, "leftCircleX", leftX, rightX);
+        leftPointAnimator.setDuration(5000L);
+        leftPointAnimator.setInterpolator(new AccelerateInterpolator(1.5F));
+        mAnimators.add(leftPointAnimator);
+
+        ObjectAnimator rightCircleRadiusAnimator = ObjectAnimator.ofFloat(this, "rightCircleRadius", mMinCircleRadius, mMaxCircleRadius);
+        rightCircleRadiusAnimator.setDuration(5000L);
+        rightCircleRadiusAnimator.setInterpolator(new AccelerateInterpolator(1.5F));
+        mAnimators.add(rightCircleRadiusAnimator);
+
+        ObjectAnimator leftCircleRadiusAnimator = ObjectAnimator.ofFloat(this, "leftCircleRadius", mMaxCircleRadius, mMinCircleRadius);
+        leftCircleRadiusAnimator.setDuration(5000L);
+        leftCircleRadiusAnimator.setInterpolator(new DecelerateInterpolator(0.8F));
+        mAnimators.add(leftCircleRadiusAnimator);
+
+        int color1 = mColors.get(position);
+        int color2 = mColors.get(next);
+        ValueAnimator paintColorAnimator = ObjectAnimator.ofInt(color1, color2);
+        paintColorAnimator.setDuration(5000L);
+        paintColorAnimator.setEvaluator(new ArgbEvaluator());
+        paintColorAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        paintColorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            public void onAnimationUpdate(ValueAnimator animator) {
+                mPaint.setColor((Integer) animator.getAnimatedValue());
+            }
+        });
+
+        mAnimators.add(paintColorAnimator);
+    }
+
+    private void seekAnimator(float offset) {
+        for (ValueAnimator animator : mAnimators) {
+            animator.setCurrentPlayTime((long) (5000.0F * offset));
+        }
+
+        postInvalidate();
+    }
+
+    public void setPositionAndOffset(int position, float offSet) {
+        createAnimator(position);
+        seekAnimator(offSet);
+    }
+
+    private void resetPoint() {
+        mPoints.clear();
+        for (int i = 0; i < mPagerCount; i++) {
+            int x = mWidth / (mPagerCount + 1) * (i + 1);
+            mPoints.add(new PointF(x, 0));
+        }
+
+        if (!mPoints.isEmpty()) {
+            leftCircleX = mPoints.get(0).x;
+            leftCircleRadius = mMaxCircleRadius;
+
+            rightCircleX = mPoints.get(0).x;
+            rightCircleRadius = mMinCircleRadius;
+            postInvalidate();
+        }
+    }
+
+    public void setPagerCount(int pagerCount) {
+        mPagerCount = pagerCount;
+    }
+
+    public void setColors(List<Integer> colors) {
+        mColors = colors;
+
+        if (!colors.isEmpty()) {
+            mPaint.setColor(colors.get(0));
+        }
+    }
+
+    public void setMode(Mode mode) {
+        this.mMode = mode;
+    }
+
+    public float getLeftCircleX() {
+        return leftCircleX;
+    }
+
+    public void setLeftCircleX(float leftCircleX) {
+        this.leftCircleX = leftCircleX;
+    }
+
+    public float getLeftCircleRadius() {
+        return leftCircleRadius;
+    }
+
+    public void setLeftCircleRadius(float leftCircleRadius) {
+        this.leftCircleRadius = leftCircleRadius;
+    }
+
+    public float getRightCircleRadius() {
+        return rightCircleRadius;
+    }
+
+    public void setRightCircleRadius(float rightCircleRadius) {
+        this.rightCircleRadius = rightCircleRadius;
+    }
+
+    public float getRightCircleX() {
+        return rightCircleX;
+    }
+
+    public void setRightCircleX(float rightCircleX) {
+        this.rightCircleX = rightCircleX;
+    }
 }
--- Simple/src/main/java/cn/gavinliu/lib/android/pagerindicator/simple/MainActivity.java
+++ Simple/src/main/java/cn/gavinliu/lib/android/pagerindicator/simple/MainActivity.java
@@ -1,35 +1,103 @@
 package cn.gavinliu.lib.android.pagerindicator.simple;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.gavinliu.lib.android.pagerindicator.DropsPagerIndicator;
 
 public class MainActivity extends ActionBarActivity {
+
+    private DropsPagerIndicator indicator;
+    private ViewPager viewPager;
+
+    private List<View> views;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        indicator = (DropsPagerIndicator) findViewById(R.id.indicator);
+
+
+        views = new ArrayList<>();
+        views.add(getLayoutInflater().inflate(R.layout.layout_pager, null));
+        views.add(getLayoutInflater().inflate(R.layout.layout_pager, null));
+        views.add(getLayoutInflater().inflate(R.layout.layout_pager, null));
+        views.add(getLayoutInflater().inflate(R.layout.layout_pager, null));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(0xFFFF0000);
+        colors.add(0xFF00FF00);
+        colors.add(0xFF0000FF);
+        colors.add(0xFFFF00FF);
+        indicator.setColors(colors);
+        indicator.setPagerCount(views.size());
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                indicator.setPositionAndOffset(position, positionOffset);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        viewPager.setAdapter(new PagerAdapter() {
+
+            @Override
+            public int getCount() {
+                return views.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(views.get(position));
+            }
+
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(views.get(position), 0);
+                return views.get(position);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
